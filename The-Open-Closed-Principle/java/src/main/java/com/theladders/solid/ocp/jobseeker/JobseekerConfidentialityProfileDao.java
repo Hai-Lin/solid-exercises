c3c7c98db87c492ebb77c055e78c9735855d73f3
Hai Lin <hai.lin.cs@gmail.com>
2013-04-19 23:21:55 -0400

    Rename JobseekerConfidentialityProfile

--- The-Open-Closed-Principle/java/src/main/java/com/theladders/solid/ocp/jobseeker/JobseekerConfidentialityProfileDao.java
+++ The-Open-Closed-Principle/java/src/main/java/com/theladders/solid/ocp/jobseeker/JobseekerConfidentialityProfileDao.java
@@ -4,8 +4,8 @@ package com.theladders.solid.ocp.jobseeker;
 public class JobseekerConfidentialityProfileDao
 {
   @SuppressWarnings("unused")
-  public JobseekerConfidentialityProfile fetchJobSeekerConfidentialityProfile(int id)
+  public JobseekerConfidentialityProfiles fetchJobSeekerConfidentialityProfile(int id)
   {
-    return new JobseekerConfidentialityProfile();
+    return new JobseekerConfidentialityProfiles();
   }
 }
